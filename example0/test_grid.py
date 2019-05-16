378549ae5bd9a614c28503431d2aab35878c3813
anabudisa <anbudisa@gmail.com>
2019-05-16 20:35:32 +0200

    small update in testing 3d operators in example0

--- example0/test_grid.py
+++ example0/test_grid.py
@@ -15,18 +15,18 @@ def make_mesh(file_name, mesh_size, plot=True):
     # g.compute_geometry()
 
     # The fractures are specified by their vertices, stored in a numpy array
-    f_1 = pp.Fracture(np.array([[0, 1, 2, 0], [0, 0, 1, 1], [0, 0, 1, 1]]))
+    f_1 = pp.Fracture(np.array([[0, 0, 1, 1], [0, 0, 1, 1], [0, 1, 1, 0]]))
+    f_2 = pp.Fracture(np.array([[1, 1, 0, 0], [0, 0, 1, 1], [0, 1, 1, 0]]))
 
     # Also define the domain
-    domain = {'xmin': -2, 'xmax': 3, 'ymin': -2, 'ymax': 3, 'zmin': -3,
-              'zmax': 3}
+    domain = {'xmin': 0, 'xmax': 1, 'ymin': 0, 'ymax': 1, 'zmin': 0, 'zmax': 1}
 
     # Define a 3d FractureNetwork, similar to the 2d one
-    network = pp.FractureNetwork3d([f_1], domain=domain)
-    mesh_args = {'mesh_size_frac': 0.3, 'mesh_size_min': 0.2}
+    network = pp.FractureNetwork3d([f_1, f_2], domain=domain)
+    mesh_args = {'mesh_size_frac': 1.75, 'mesh_size_min': 1.7}
 
     # Generate the mixed-dimensional mesh
-    gb = network.mesh(mesh_args, ensure_matching_face_cell=False)
+    gb = network.mesh(mesh_kwargs, ensure_matching_face_cell=False)
 
     # make fracture network
     # network_3d = pp.fracture_importer.network_3d_from_csv(file_name)
@@ -58,4 +58,4 @@ if __name__ == "__main__":
     # grid config file
     name = "network_3d.csv"
 
-    make_mesh(name, 1.7)
+    make_mesh(name, 1.2)
